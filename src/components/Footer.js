fcfceb948f23eb8e613bd5f8887c69dc60655561
Cyndzi <cyndzi@Cyndzis-MacBook-Pro.local>
2022-12-09 15:44:02 +0100

    footer adjusted to the bottom of the page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <>
         <Box style={{
-            position: 'absolute',
+            position: 'relative',
             bottom: 0,
             width: '100%',
             height: '2.5rem'}}>
